627bb2406dd4e4de474d6ad9946038386dbdefb4
sanjay5577 <sanjaybms17@gmail.com>
2024-08-20 21:14:43 +0530

    invoice backend

--- index.js
+++ index.js
@@ -1,3 +1,38 @@
-// run `node index.js` in the terminal
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const passport = require("passport");
+const authRoute = require("./routes/auth");
+const cookieSession = require("cookie-session");
+const passportStrategy = require("./passport");
+const app = express();
 
-console.log(`Hello Node.js v${process.versions.node}!`);
+app.use(
+	cookieSession({
+		name: "session",
+		keys: ["cyberwolve"],
+		maxAge: 24 * 60 * 60 * 1000,
+	})
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.use(
+	cors({
+		origin: "https://invoice-remainder-frontend.vercel.app",
+		credentials: true,
+	})
+);
+
+console.log("hi")
+
+app.get('/', (req, res) => {
+	res.send('Hello World!');
+  });
+
+app.use("/auth", authRoute);
+
+
+const port = process.env.PORT || 8080;
+app.listen(port, () => console.log(`Listenting on port ${port}...`));
---
+++ routes/auth.js
@@ -0,0 +1,44 @@
+const router = require("express").Router();
+const passport = require("passport");
+
+console.log("yes")
+
+router.get("/login/success", (req, res) => {
+	console.log("yes2")
+	if (req.user) {
+        res.json({
+            success: true,
+            user: req.user,
+            message: "User has successfully authenticated",
+        });
+    } else {
+        res.status(401).json({
+            success: false,
+            message: "User is not authenticated",
+        });
+    }
+});
+
+router.get("/login/failed", (req, res) => {
+	res.status(401).json({
+		error: true,
+		message: "Log in failure",
+	});
+});
+
+router.get("/google", passport.authenticate("google", ["profile", "email"]));
+
+router.get(
+	"/google/callback",
+	passport.authenticate("google", {
+		successRedirect: process.env.CLIENT_URL,
+		failureRedirect: "/login/failed",
+	})
+);
+
+router.get("/logout", (req, res) => {
+	req.logout();
+	res.redirect(process.env.CLIENT_URL);
+});
+
+module.exports = router;
